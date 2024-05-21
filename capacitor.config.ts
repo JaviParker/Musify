99df63a3b449b80e5da4aecf7b29970de12dace7
JaviParker <91238693+JaviParker@users.noreply.github.com>
2024-05-21 13:37:41 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'StarWars',
+  webDir: 'www'
+};
+
+export default config;
